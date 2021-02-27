c4a34c3eb94b3b12ce3708cadd671fa3a4192d65
Ryan Bannon <paris.moore@hotmail.com>
2021-02-27 12:44:35 +0000

    first commit 2702

---
+++ src/test/java/com/vogella/maven/eclipse/AppTest.java
@@ -0,0 +1,15 @@
+package com.vogella.maven.eclipse;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
